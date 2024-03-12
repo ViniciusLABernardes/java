681315bd0de99610ae5c3c55f20a86b14165de0e
Vinicius Leandro <111243990+ViniciusLABernardes@users.noreply.github.com>
2024-03-11 22:50:54 -0300

    aula5 java

---
+++ aula05/src/aula05/FolhaPag.java
@@ -0,0 +1,27 @@
+package aula05;
+
+class Pagamento{
+		String descritivo;
+		double valor;
+		
+		double setValor(double valor) {
+		 this.valor = valor;
+		 return valor;
+		}
+	}
+
+//	class Holerite{
+	//	Pagamento pagamento;
+	//	Desconto descontos;
+//	}
+	
+public class FolhaPag {
+	
+	
+	public static void main(String args) {
+		//Holerite holerite = new Holerite ();
+		//holerite.descontos.setDesc(20);
+		//holerite.pagamento.setValor(5000);
+	}
+		
+}
---
+++ aula05/src/aula05/umArrayList.java
@@ -0,0 +1,16 @@
+package aula05;
+
+import java.util.ArrayList;
+
+public class umArrayList {
+
+		public static void main(String[] args) {
+			ArrayList<Integer> inteiros = new ArrayList();
+			
+			inteiros.add(21);
+			inteiros.add(44);
+		//	inteiros.add("diego");
+			
+			System.out.println(inteiros);
+		}
+}
