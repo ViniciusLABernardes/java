c8f3e01f83ed57a3f4aa7cd334f038934ceb1562
Vinicius Leandro <111243990+ViniciusLABernardes@users.noreply.github.com>
2024-04-01 22:57:48 -0300

    Add files via upload

---
+++ FiapOnline/src/pacote/Main.java
@@ -0,0 +1,16 @@
+package pacote;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Bem vindo professor! Digite seu nome: ");
+		String nome = leitor.nextLine();
+		
+		leitor
+		
+		
+	}
+}
